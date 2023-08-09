4b40f37d2067c4d9d129a9770dc3dd55c30aca81
luctfyal12 <luctfyalkatiri08@gmail.com>
2023-08-09 20:34:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// JAVA
+
+// int x = 5;
+// string y = 'abc';
+
+// JavaScript
+
+// let x = 5;
+// let name = 'abc';
+
+let a = 1;
+console.log(a/0);
+
+let b = 'not a number';
+console.log(b/0)
+
+let c = "jum'at";
+console.log(c);
+
+let d = `${1 + 2}`;
+console.log(d);
+
+let e = 5;
+console.log(`umur saya adalah ${d*e} tahun.`)
+
+//  boolean
+let f = 10 > 8;
+console.log(f);
+console.log(e < f);
+
+let name
+console.log(name);
+
+// object
+const student = {
+    firsName: 'Luctfy',
+    lastName: 'Alkatiri',
+    class: 12
+};
+student.age = 18; //Menambahkan object data
+console.log(student)
+
+// Array 
+// let arr = new Array();
+// let arr = [];
+
+let fruits = ['Mango','Dragonfruits','Apple'];
+fruits.push = 'Banana'; //Menambahkan value
+console.log(fruits)
+
+// Typeof 
+let p = 'ram'
+typeof(p); 
+console.log(typeof(p));
+
+// comparison operator 
+console.log(3 >= 3)
+console.log(3 == 1) 
+console.log(3 != 2)
+// == sama dengan (untuk persamaan)
+// != tidak sama dengan
+// jika menggunakan ==, angka 0 akan dianggap false
+console.log(0 == false)
+console.log(0 == true)
+console.log('' == false)
+// jika menggunakan === (identik), angka 0 akan berbeda dengan false
+console.log(0 === false)
+console.log('' === true)
+
+// logical operator
+
+
+
+ 
